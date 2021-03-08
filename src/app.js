f179bf81adeef9916a1dfe1ec7ad8126a5db92b9
Zsolt Leitner <dxtrlbrtry@yahoo.com>
2021-03-08 15:13:13 +0200

    extracted configs

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 'use strict';
 
 const express = require('express');
+
 const user = require('../lib/endpoints/users');
 const testObject = require('../lib/endpoints/testObjects');
-const DbService = require('../lib/dbService').DbService
-// Constants
-const PORT = 1234;
-const HOST = '0.0.0.0';
-const app = express();
 
-exports.db = new DbService({
-    'host': '172.19.0.2',
-    'port': 33060,
-    'user': 'root',
-    'password': 'password'
-})
+const appConfig = require('./appConfig')
+
+const app = express();
 
 app.use(express.json());
 
@@ -26,5 +19,5 @@ app.get('/test/', testObject.getObjects);
 app.post('/test', testObject.addObject);
 app.delete('/test/', testObject.deleteObject)
 
-app.listen(PORT, HOST);
-console.log('Running on http://' + HOST + ':' + PORT);
+app.listen(appConfig.port, appconfig.host);
+console.log('Running on http://' + appconfig.host + ':' + appconfig.port);
