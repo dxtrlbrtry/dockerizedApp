e033fd820bfca2cf9125704fa39a616294065524
Zsolt Leitner <dxtrlbrtry@yahoo.com>
2021-03-11 01:19:52 +0200

    commit test file

---
+++ tests/testFile.js
@@ -0,0 +1,29 @@
+var mysqlx = require('@mysql/xdevapi');
+var request = require('request-promise')
+var dbService = require('../lib/dbService')
+
+function createSession() {
+    args = process.argv.slice(2);
+    return mysqlx.getSession({
+      "host": argv[0],
+      "user": "root",
+      "password": "password",
+      "database": "db"
+    })
+  }
+
+(async () => { await createSession()
+        .then(session => {
+            console.log("db success")
+        })
+        .catch(err => console.log(err));
+        const options = {
+            method: 'GET',
+            url: argv[1] + '/users/',
+            json: false,
+            resolveWithFullResponse: false
+          }
+          await request(options)
+            .then(resp => { console.log(JSON.parse(resp))})
+            .catch(err => console.log(err));
+    })();
