99b390d699c431964ef7ceee9a64a52218fc3d4c
Zsolt Leitner <dxtrlbrtry@yahoo.com>
2021-03-12 18:54:39 +0200

    log error on server listener

--- src/endpoints/testObjects.js
+++ src/endpoints/testObjects.js
@@ -6,19 +6,28 @@ exports.getObjects = function(req, res) {
   logger.log(`GET request received at /test/`)
   db.getTable(schemas.testObject)
     .then(result => res.send({ statusCode: 200, result: result }))
-    .catch(err => res.send({ statusCode: 400, 'error: ': err }));
+    .catch(err => {
+      logger.error(err);
+      res.send({ statusCode: 400, 'error: ': err });
+    })
   };
   
 exports.addObject = function(req, res) {
   logger.log(`POST request received at /test/`)
   db.insertItem(schemas.testObject, req.body)
     .then(result =>  res.send({ statusCode: 202, result: result }))
-    .catch(err => res.send({ statusCode: 400, 'error: ': err }));
+    .catch(err => {
+      logger.error(err);
+      res.send({ statusCode: 400, 'error: ': err });
+    })
   }
   
 exports.deleteObject = function(req, res) {
   logger.log(`DELETE request received at /test/`)
   db.deleteItem(schemas.testObject, req.body)
     .then(result => res.send({ statusCode: 200, result: result })) 
-    .catch(err => res.send({ statusCode: 400, 'error: ': err }));
+    .catch(err => {
+      logger.error(err);
+      res.send({ statusCode: 400, 'error: ': err });
+    })
   }
--- src/endpoints/users.js
+++ src/endpoints/users.js
@@ -6,19 +6,28 @@ exports.getUsers = function(req, res) {
   logger.log(`GET request received at /users/`)
   db.getTable(schemas.user)
     .then(result => res.send({ statusCode: 200, result: result }))
-    .catch(err => res.send({ statusCode: 400, 'error: ': err }));
+    .catch(err => {
+      logger.error(err);
+      res.send({ statusCode: 400, 'error: ': err });
+    })
   };
   
 exports.addUser = function(req, res) {
   logger.log(`POST request received at /users/`)
   db.insertItem(schemas.user, req.body)
     .then(result =>  res.send({ statusCode: 202, result: result }))
-    .catch(err => res.send({ statusCode: 400, 'error: ': err }));
+    .catch(err => {
+      logger.error(err);
+      res.send({ statusCode: 400, 'error: ': err });
+    })
   }
   
 exports.deleteUser = function(req, res) {
   logger.log(`DELETE request received at /users/`)
   db.deleteItem(schemas.user, req.body)
-    .then(result => res.send({ statusCode: 200, result: result })) 
-    .catch(err => res.send({ statusCode: 400, 'error: ': err }));
+    .then(result => res.send({ statusCode: 200, result: result }))
+    .catch(err => {
+      logger.error(err);
+      res.send({ statusCode: 400, 'error: ': err });
+    })
   }
